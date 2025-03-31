9727a5a722b69e8d7d7900191b6b4bdbc6ca7e28
kutho53 <kurtisthomas53@gmail.com>
2025-03-31 00:33:56 -0400

    comments

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const cell = document.getElementsByClassName('cell');
 const players = ['X', 'O'];
 let currentPlayer = players[0];
 
+//create empty array of current cells for each player
 const xBoard = [];
 const oBoard = [];
 
@@ -17,15 +18,16 @@ const winningCombinations = [
     ["2", "4", "6"]
 ]
 
-// when button click make x/o
-    //function to make sure button is empty, lock it after using
 function markButton(cellPosition) {
     const cell = document.getElementById(cellPosition);
+    //abort function if cell is not empty
+    //Change the innerHTML of the cell to display Player ID
     if (cell.innerHTML !== ''){
         return;
     }
     cell.innerHTML = currentPlayer;
 
+    //push current cell to player's array and check if player has won
     if (currentPlayer === 'X'){
         xBoard.push(cellPosition);
         if (winChecker(xBoard)){
@@ -43,17 +45,18 @@ function markButton(cellPosition) {
     console.log(xBoard);
     console.log(oBoard);
 
+    //Alerts players that they have tied
     if (xBoard.length + oBoard.length === 9) {
         alert("Cat's game! Start over to play again.")
     }
 }
 // check if winning combo
+//iterates through each subArray of the winningCombinations array, labeling each one a 'combo'
+//for each combo, checks if all cells are present in the selected board, if yes, returns a boolean 'true'
 function winChecker(board){
     for (let combo of winningCombinations) {
-    //iterates through each subArray of the winningCombinations array, labeling each one a 'combo'
         if (combo.every(cell => board.includes(cell)))
             return true;
-        //for each combo, checks if all cells are present in the selected board, if yes, returns a boolean 'true'
     } 
     return false;
 }
